7fa265c61f21a1d599fcedc1d4983860b616d4e3
chilin_wang <chilin_wang@asus.com>
2016-06-15 15:33:10 +0800

    Init

---
+++ app/src/androidTest/java/com/example/chilin_wang/testleakcanary/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.chilin_wang.testleakcanary;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
